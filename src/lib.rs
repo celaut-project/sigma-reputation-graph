0eddbcbc950de4ce7671bd1ed9c66ba566fbccb2
jossemii <jossemii@proton.me>
2023-11-30 21:05:26 +0100

    some improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+use std::fmt::{Debug, Formatter};
+use std::ptr;
+use rand::Rng;
 
 mod test;
 
-#[derive(PartialEq)]
+fn generate_random_vec_u8(length: usize) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    let mut vec_u8: Vec<u8> = Vec::new();
+
+    for _ in 0..length {
+        vec_u8.push(rng.gen());
+    }
+
+    vec_u8
+}
+
+
+
+#[derive(PartialEq, Debug)]
 enum PointerBox<'a> {
     ReputationProof(&'a ReputationProof<'a>),
     String(String)
@@ -22,12 +38,18 @@ impl<'a> PartialEq for ReputationProof<'a> {
     }
 }
 
+impl<'a> Debug for ReputationProof<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ReputationProof {{ box id: {:?}", self.box_id)
+    }
+}
+
 impl <'a, 'b> ReputationProof<'a> {
     fn new(
         box_id: Vec<u8>,
         token_id: Vec<u8>,
         total_amount: i64,
-        outputs: Vec<&'a ReputationProof<'a>>,
+        outputs: Vec<&'b ReputationProof<'a>>,
         pointer_box: Option<&'b PointerBox<'a>>,
     ) -> ReputationProof<'b> {
         ReputationProof {
@@ -50,7 +72,7 @@ impl <'a, 'b> ReputationProof<'a> {
         *   Supposed that ->  'b < 'a
          */
         return ReputationProof::new(
-            vec![], vec![],
+            generate_random_vec_u8(16), vec![],
             total_amount,  vec![],
             pointer_box
         )
@@ -93,8 +115,21 @@ impl <'a, 'b> ReputationProof<'a> {
         Compute the reputation of a pointer searching on all the output tree.
     */
     pub fn compute(&self, pointer: Option<&'b PointerBox<'a>>) -> f64 {
+        /*
+                This configuration don't allow to have assigned reputation and delegated reputation
+                at the same time.
+         */
         if self.pointer_box.is_some() {
             // Recursive case: if there is pointer, uses the pointer_box's reputation.
+
+            println!("{:?}", ptr::addr_of!(self.pointer_box));
+            println!("{:?}", self.pointer_box.unwrap());
+            println!("------");
+            println!("{:?}", ptr::addr_of!(pointer));
+            println!("{:?}", self.pointer_box.unwrap());
+            println!("-----");
+            println!("{}", self.pointer_box == pointer);  // Says false but should be true.
+
             if pointer.is_some() && self.pointer_box == pointer {
                 1.00
             } else {
--- src/test.rs
+++ src/test.rs
@@ -6,16 +6,16 @@ mod tests {
     #[test]
     fn test_reputation_graph() {
 
-        let owner_obj1 = ReputationProof::create(100, None);
-        let pointer1 = PointerBox::ReputationProof(&owner_obj1);
+        let object1 = ReputationProof::create(100, None);
+        let pointer1 = PointerBox::ReputationProof(&object1);
 
-        let owner_obj2 = ReputationProof::create(100000000, None);
-        let pointer2 = PointerBox::ReputationProof(&owner_obj2);
+        let object2 = ReputationProof::create(100000000, None);
+        let pointer2 = PointerBox::ReputationProof(&object2);
 
         let pointer3 = PointerBox::String(String::from("nodo-c57"));
 
         // proof 1
-        let mut proof1 = ReputationProof::create(100, None);
+        let mut proof1 = ReputationProof::create(100, Some(&pointer2));
 
         let mut proof2 = (&proof1).spend(60, None);
         proof1.outputs.push(&proof2);
@@ -24,16 +24,19 @@ mod tests {
         proof1.outputs.push(&proof3);
 
         // proof 2
-        /*let (mut proof2, _) = proof2.spend(30, Some(&pointer1));
+        /*
+        let (mut proof2, _) = proof2.spend(30, Some(&pointer1));
         let (mut proof2, _) = proof2.spend(30, Some(&pointer2));
 
         // proof 3
         let (mut proof3, _) = proof3.spend(7, Some(&pointer3));
+        */
 
-        assert_eq!((&proof1).compute(Some(&pointer1)), 0.00);
-        assert_eq!((&proof1).compute(Some(&pointer2)), 0.00);
-        assert_eq!((&proof1).compute(Some(&pointer3)), 0.00);
+        assert_eq!((&proof1).compute(Some(&pointer1)), 0.1);
+        assert_eq!((&proof1).compute(Some(&pointer2)), 0.60);
+        // assert_eq!((&proof1).compute(Some(&pointer3)), 0.00);
 
+        /*
         assert_eq!((&proof2).compute(Some(&pointer1)), 0.00);
         assert_eq!((&proof2).compute(Some(&pointer2)), 0.00);
         assert_eq!((&proof2).compute(Some(&pointer3)), 0.00); */
