478024b6853fcdbe19a5218a07061b59c4bb72c9
jossemii <jossemii@proton.me>
2023-12-03 20:21:41 +0100

    pyo3 functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fmt::{Debug, Formatter};
 use rand::Rng;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use std::cmp::Ordering;
 
 fn generate_random_vec_u8(length: usize) -> Vec<u8> {
@@ -155,22 +156,36 @@ impl <'a, 'b> ReputationProof<'a> {
 
 
 #[pyfunction]
-fn static_compute()
+fn spend()
 {
-    println!("Static compute function");
+    /*
+        Params
+        - Ergo node url
+        - SurrealDB endpoint  https://surrealdb.com/docs/embedding/rust#connect
+        - proof
+        - amount
+        - pointer
+    */
+    println!("Spend function.");
 }
 
 #[pyfunction]
-fn dynamic_compute()
+fn compute()
 {
-    println!("Dynamic compute function.");
+    /*
+        Params
+        - Ergo node url
+        - SurrealDB endpoint
+        - pointer to calculate
+    */
+    println!("Compute function.");
 }
 
 
 #[pymodule]
 fn compute_reputation_graph(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(static_compute, m)?)?;
-    m.add_function(wrap_pyfunction!(dynamic_compute, m)?)?;
+    m.add_function(wrap_pyfunction!(spend, m)?)?;
+    m.add_function(wrap_pyfunction!(compute, m)?)?;
 
     Ok(())
 }
