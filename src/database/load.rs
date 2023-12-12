bdc363c82e111915a77c11416c75f662d5f9340b
jossemii <jossemii@proton.me>
2023-12-12 20:31:14 +0100

    load.rs

--- src/database/load.rs
+++ src/database/load.rs
@@ -1,6 +1,36 @@
+use std::fmt::Debug;
+use std::io::Error;
+use surrealdb::Surreal;
+use serde::{Serialize, Deserialize};
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::sql::{Thing, Operation};
 
 
-pub fn compute_from_db(proof_id: String) -> Result<f64, std::io::Error>
+const DB_ERROR_MSG: &str = "Invalid response or error connection from the database";
+const NAMESPACE: &str = "local";
+const DATABASE: &str = "graph";
+const ENDPOINT: &str = "127.0.0.1:8000";
+const RESOURCE: &str = "reputation_proof";
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ReputationProof {
+    previous_proof_id: Option<String>,
+    amount: i64
+}
+
+#[tokio::main]
+pub async fn compute_from_db(proof_id: String) -> Result<f64, std::io::Error>
 {
+    let db = Surreal::new::<Ws>(ENDPOINT)
+        .await.expect(DB_ERROR_MSG);
+        
+    db.use_ns(NAMESPACE).use_db(DATABASE).await.expect(DB_ERROR_MSG);
+
+    println!("Id -> {:?}", proof_id);
+    
+    let response: Option<ReputationProof> = db.select((RESOURCE, proof_id)).await.expect(DB_ERROR_MSG);
+
+    println!("Response -> {:?}", response);
     Ok(1.00)
 }
