227c1a42bdff305f54ed1d1629df4f237e3d2b7b
jossemii <jossemii@proton.me>
2023-12-26 19:10:25 +0100

    using async recursive

--- src/database/load.rs
+++ src/database/load.rs
@@ -1,10 +1,12 @@
 use std::fmt::Debug;
+use std::future::Future;
 use std::io::Error;
-use std::thread;
+use std::pin::Pin;
 use serde::{Serialize, Deserialize};
-use surrealdb::engine::remote::ws::Ws;
+use surrealdb::engine::remote::ws::{Client, Ws};
 use surrealdb::sql::{Thing};
 use surrealdb::Surreal;
+use tokio::runtime::Handle;
 use crate::proof::ReputationProof;
 
 
@@ -20,6 +22,60 @@ struct ReputationProofDB {
     amount: i64
 }
 
+fn recursive(proof_id: String, db: Surreal<Client>) -> Pin<Box<dyn Future<Output = Result<ReputationProof<'static>, Error>>>>
+{
+    //  Why Box::pin? ->  https://doc.rust-lang.org/error_codes/E0733.html
+    Box::pin(async move {
+        println!("Id -> {:?}", proof_id);
+
+        let response: Option<ReputationProofDB> = {
+            db.select((RESOURCE, proof_id.to_string())).await.expect(DB_ERROR_MSG)
+        };
+
+        println!("Response -> {:?}", response);
+
+        match response  {
+            Some(r) => {
+                let proof = ReputationProof::create(Vec::new(),
+                                                    r.amount, None);
+
+                // TODO Should be ->  let mut query: String = "SELECT ->leaf.out FROM reputation_proof:".to_owned();
+                let mut query: String = "SELECT out FROM leaf WHERE in = reputation_proof:".to_owned();
+                query.push_str(&*proof_id.to_string());
+                let mut dependencies_response = db.query(query)
+                    .await.expect(DB_ERROR_MSG);
+
+                let dependencies: Vec<Thing> = dependencies_response.take("out").expect(DB_ERROR_MSG);
+                for dependency in dependencies {
+                    let dependency_id = dependency.id.to_raw();
+                    println!("dependency -> {:?}\n", dependency_id);
+
+                    match recursive(dependency_id, db.clone()).await {
+                        Ok(r) => println!("Reputation proof -> {:?}", r),
+                        Err(err) => eprintln!("{:?}", err)
+                    }
+                }
+
+                /* PSEUDO CODE
+
+                for dependency_id in dependencies:
+                     if let dependency = load_from_db(dependency_id)
+                     {
+                        if (&proof).can_be_spend(dependency.unwrap().amount) {
+                            proof.outputs.push(d);
+                        }
+                     }
+                     else {
+                        todo!();
+                     }
+                 */
+                Ok(proof)
+            },
+            None => todo!(),
+        }
+    })
+}
+
 #[tokio::main]
 pub async fn load_from_db(proof_id: String) -> Result<ReputationProof<'static>, Error>
 {
@@ -28,55 +84,8 @@ pub async fn load_from_db(proof_id: String) -> Result<ReputationProof<'static>, 
 
     db.use_ns(NAMESPACE).use_db(DATABASE).await.expect(DB_ERROR_MSG);
 
-    println!("Id -> {:?}", proof_id);
-
-    let response: Option<ReputationProofDB> = {
-        db.select((RESOURCE, proof_id.to_string())).await.expect(DB_ERROR_MSG)
-    };
-
-    println!("Response -> {:?}", response);
-
-    match response  {
-        Some(r) => {
-            let proof = ReputationProof::create(Vec::new(),
-                                                r.amount, None);
-
-            // TODO Should be ->  let mut query: String = "SELECT ->leaf.out FROM reputation_proof:".to_owned();
-            let mut query: String = "SELECT out FROM leaf WHERE in = reputation_proof:".to_owned();
-            query.push_str(&*proof_id.to_string());
-            let mut dependencies_response = db.query(query)
-                .await.expect(DB_ERROR_MSG);
-
-            let dependencies: Vec<Thing> = dependencies_response.take("out").expect(DB_ERROR_MSG);
-            for dependency in dependencies {
-                println!("\n\n Go to load from db -> {:?}", dependency);
-
-                load_from_db(dependency.id.to_raw());
-
-                /*thread::spawn(move || {
-                    // TODO PERFORMANCE -> https://stackoverflow.com/questions/62536566/how-can-i-create-a-tokio-runtime-inside-another-tokio-runtime-without-getting-th
-                    match load_from_db(dependency.id.to_raw()) {
-                        Ok(r) => {
-                            println!("{:?}", r);
-                        },
-                        Err(_) => println!("Errr")
-                    }
-                }).join().expect("Thread panicked");*/
-            }
-
-            /* for dependency_id in dependencies:
-                 if let dependency = load_from_db(dependency_id)
-                 {
-                    if (&proof).can_be_spend(dependency.unwrap().amount) {
-                        proof.outputs.push(d);
-                    }
-                 }
-                 else {
-                    todo!();
-                 }
-             */
-            Ok(proof)
-        },
-        None => todo!(),
+    match recursive(proof_id, db).await {
+        Ok(r) => Ok(r),
+        Err(err) => Err(err)
     }
 }
