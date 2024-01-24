ddb3015137690624860289623f9a5c71291bf92c
jossemii <jossemii@proton.me>
2024-01-24 18:45:01 +0100

    fixed db name

--- src/database/global.rs
+++ src/database/global.rs
@@ -4,7 +4,7 @@ use surrealdb::sql::Thing;
 pub(crate) const DB_ERROR_MSG: &str = "Invalid response or error connection from the database";
 pub(crate) const NAMESPACE: &str = "local";
 pub(crate) const DATABASE: &str = "graph";
-pub(crate) const ENDPOINT: &str = "/home/jse/Projects/compute-reputation-graph/reputation.db";
+pub(crate) const ENDPOINT: &str = "reputation.db";
 pub(crate) const RESOURCE: &str = "reputation_proof";
 
 #[derive(Debug, Serialize, Deserialize)]
